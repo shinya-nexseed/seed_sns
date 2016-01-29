56ae6684e70b7b34b81c1eda4510b99e95010e77
shinyahirai <908.shinya@gmail.com>
2016-01-29 12:30:33 +0800

    ログアウト

---
+++ logout.php
@@ -0,0 +1,24 @@
+<?php
+    // 定型文なので、あまり中の意味まで説明、理解する必要ない
+    // http://php.net/manual/ja/function.session-destroy.php
+
+    session_start();
+
+    // セッション情報を削除
+    $_SESSION = array();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+          $params["path"], $params["domain"],
+          $params["secure"], $params["httponly"]
+        );
+    }
+    session_destroy();
+
+    // Cookie情報を削除
+    setcookie('email', '', time() - 3600);
+    setcookie('password', '', time() - 3600);
+
+    header('Location: login.php');
+    exit();
+?>
